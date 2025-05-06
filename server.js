a9bdcc9bbdf974971326c3d459b417165a9a50ed
sahdan17 <sahdan.wira.17@gmail.com>
2025-05-06 08:42:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
-    console.log(`🔥 Chatbot Server: System => Started on :${PORT}`)
+    console.log(`Chatbot Server: System => Started on :${PORT}`)
 })
